88981b8b630c0a911de85cf2a16e33fb935f9a0a
khomotso gojela <motsoak49@gmail.com>
2023-08-03 00:57:05 +0200

    importing scripts challenge 2

--- challenge_2/scripts.js
+++ challenge_2/scripts.js
@@ -1,5 +1,9 @@
+import './alex.js'
+import './johannes.js'
+import './nwabisa.js'
+
 // scripts.js
 
 // console.log('Roles:', nwabisa.role, johanes.role, alex.role)
 
-console.log('Roles:, nwabisa.role, johanes.role, alex.role')
+console.log('Roles: ', nwabisa.role, johanes.role, alex.role)
