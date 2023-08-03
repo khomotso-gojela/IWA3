564df16c482c9e689d86cf9aabeae70c8b59c3b5
khomotso gojela <motsoak49@gmail.com>
2023-08-03 08:28:28 +0200

    adding roles

--- challenge_2/nwabisa.js
+++ challenge_2/nwabisa.js
@@ -8,7 +8,7 @@
 
 const firstname = "Nwabisa"
 const surname = "Gabe"
-const role = "CEO"
+export const role = "CEO"
 
 const display= firstname + " " + surname + " (" + role + ")"
 document.querySelector('#nwabisa').innerText = display
--- challenge_2/scripts.js
+++ challenge_2/scripts.js
@@ -1,9 +1,12 @@
 import './alex.js'
 import './johannes.js'
 import './nwabisa.js'
+import {role as nwabisa_role} from './nwabisa.js';
+import {role as johannes_role} from './johannes.js';
+import {role as alex_role} from './alex.js';
 
 // scripts.js
 
 // console.log('Roles:', nwabisa.role, johanes.role, alex.role)
 
-console.log('Roles: ', nwabisa.role, johanes.role, alex.role)
+console.log('Roles: ', nwabisa_role,', ', johannes_role,', ', alex_role)
